c5ba95969b9fad5ba35568a03094d970e1da67f8
Rambabu <rambabu@example.com>
2023-02-27 06:21:32 +0000

    Read More/Less using hooks

---
+++ src/components/ReadMore/styledComponents.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`
+export const Title = styled.h1`
+  font-family: 'Roboto';
+  font-size: 30px;
+  font-weight: bold;
+  color: #1e293b;
+`
+export const Para = styled.p`
+  font-family: 'Roboto';
+  font-size: 20px;
+  font-weight: bold;
+  color: #334155;
+`
+export const Image = styled.img`
+  height: 400px;
+  width: 400px;
+`
+export const Paragraph = styled.p`
+  font-family: 'Roboto';
+  font-size: 20px;
+  font-weight: bold;
+  color: #334155;
+`
+export const Button = styled.button`
+  background-color: #1f81ff;
+  color: #ffffff;
+  border-radius: 8px;
+  font-size: 15px;
+`
